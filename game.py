93c4e87b0419b2dce0e7ba615db2f435badd4d36
ec1302 <ec1302@georgetown.edu>
2024-10-18 21:08:07 -0500

    Got physics collisions working on tiles and player.

--- game.py
+++ game.py
@@ -2,8 +2,8 @@ import pygame
 import sys
 # Scripts
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image
-
+from scripts.utils import load_image, load_images
+from scripts.tilemap import TileMap
 
 class Game:
     def __init__(self):
@@ -13,24 +13,35 @@ class Game:
         pygame.display.set_caption('Ninja Game')
         # Screen resolution
         self.screen = pygame.display.set_mode((640, 480))
-        
+        # Display is half the resolution of the screen
+        # Rendering small then scaling up creates pixel art effect
+        self.display = pygame.Surface((320, 240))
+
         self.clock = pygame.time.Clock()
 
         self.movement = [False, False]
 
         self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png')
         }
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
-    
+
+        self.tilemap = TileMap(self, tile_size=16)
+
     def run(self):
         while True:
             # Wipe the screen every frame
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
 
-            self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.tilemap.render(self.display)
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -48,7 +59,9 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-            
+
+            # Display is blit onto screen 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
             pygame.display.update()
             self.clock.tick(60)   
 
--- scripts/entities.py
+++ scripts/entities.py
@@ -8,13 +8,47 @@ class PhysicsEntity:
         self.size = size
         self.velocity = [0, 0]
 
-    def update(self, movement=(0, 0)):
+    # Generate a rect for physics collisions
+    def rect(self):
+        # Position is actually top left of entity, not center
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def update(self, tilemap, movement=(0, 0)):
         # Create a vector that indicates how much the entity should move in that frame
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        
+        # Calculating gravity (y axis)
+        # min() clamps the velocity which simulates terminal velocity
+        # otherwise the accelaration would not stop 
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+        
         # Update x position
         self.pos[0] += frame_movement[0]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                # Detecting collision
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                # After calculating rect collision above, we update 
+                # entity pos to rect pos
+                # pygame.Rects only work with integers, cannot have subpixel movement
+                # pygame-ce does have Frects that can handle floats
+                self.pos[0] = entity_rect.x
+
         # Update y position
         self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                # Detecting collision
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                self.pos[1] = entity_rect.y
 
     def render(self, surf):
         surf.blit(self.game.assets['player'], self.pos)
